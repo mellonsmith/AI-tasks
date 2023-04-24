d44bf004e66cc817545213f0dfbb3dee0195816a
HerrSchltn <luisscholten123@gmail.com>
2023-04-24 13:46:23 +0200

    fixed breadth_first_search

--- P01/task01/graph.py
+++ P01/task01/graph.py
@@ -43,3 +43,60 @@ class Graph:
          t.add_row([node.name] + edge_values)
       print(t)
    
+romania = Graph( ['Or', 'Ne', 'Ze', 'Ia', 'Ar', 'Si', 'Fa',
+ 'Va', 'Ri', 'Ti', 'Lu', 'Pi', 'Ur', 'Hi',
+ 'Me', 'Bu', 'Dr', 'Ef', 'Cr', 'Gi'],
+[
+   ('Or', 'Ze', 71), ('Or', 'Si', 151),
+   ('Ne', 'Ia', 87), ('Ze', 'Ar', 75),
+   ('Ia', 'Va', 92), ('Ar', 'Si', 140),
+   ('Ar', 'Ti', 118), ('Si', 'Fa', 99),
+   ('Si', 'Ri', 80), ('Fa', 'Bu', 211),
+   ('Va', 'Ur', 142), ('Ri', 'Pi', 97),
+   ('Ri', 'Cr', 146), ('Ti', 'Lu', 111),
+   ('Lu', 'Me', 70), ('Me', 'Dr', 75),
+   ('Dr', 'Cr', 120), ('Cr', 'Pi', 138),
+   ('Pi', 'Bu', 101), ('Bu', 'Gi', 90),
+   ('Bu', 'Ur', 85), ('Ur', 'Hi', 98),
+   ('Hi', 'Ef', 86)
+] )
+
+class Queue:
+    def __init__(self, edges):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0,item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+    
+    def fifo(self):
+        return self.items[-1]
+    def lifo(self):
+        return self.items[0]
+    def priority(self):
+        return self.items[0]
+    
+
+def breadth_first_search(graph, start, end):
+    queue = Queue([])
+    queue.enqueue([start])
+    while queue:
+        path = queue.dequeue()
+        node = path[-1]
+        if node == end:
+            return path
+        for edge in graph.nodes[next((i for i,v in enumerate(graph.nodes) if v.name == node), -1)].edges:
+            new_path = list(path)
+            new_path.append(edge.end.name)
+            queue.enqueue(new_path)
+    return None
+
+print(breadth_first_search(romania, 'Bi', 'Ti'))
